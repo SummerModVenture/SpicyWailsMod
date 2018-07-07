3ab34e8ea5dbdfcf2fc657d920d01df1309f79d7
Leo Yang <leoyang33@yahoo.com>
2018-07-07 18:47:02 -0400

    Make Minecraft Great Again

---
+++ src/main/java/com/wailord444/wailsmod/armor/ArmorBase.java
@@ -0,0 +1,27 @@
+package com.wailord444.wailsmod.armor;
+
+import com.wailord444.wailsmod.Main;
+import com.wailord444.wailsmod.init.ModItems;
+import com.wailord444.wailsmod.util.IHasModel;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ArmorBase extends ItemArmor implements IHasModel
+{
+    public ArmorBase(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn)
+    {
+        super(materialIn, renderIndexIn, equipmentSlotIn);
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(CreativeTabs.TOOLS);
+
+        ModItems.ITEMS.add(this);
+    }
+
+    @Override
+    public void registerModels()
+    {
+        Main.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+}
--- src/main/java/com/wailord444/wailsmod/init/ModItems.java
+++ src/main/java/com/wailord444/wailsmod/init/ModItems.java
@@ -3,13 +3,19 @@ package com.wailord444.wailsmod.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.wailord444.wailsmod.armor.ArmorBase;
 import com.wailord444.wailsmod.items.ItemBase;
 import com.wailord444.wailsmod.items.tools.*;
 
+import com.wailord444.wailsmod.util.Reference;
+import com.wailord444.wailsmod.util.handlers.SoundsHandler;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class ModItems {
@@ -19,6 +25,7 @@ public class ModItems {
 	public static final ToolMaterial MATERIAL_MEME = EnumHelper.addToolMaterial("material_meme", 2, 420, 5.0F, 2.0F, 10);
 	public static final ToolMaterial MATERIAL_ONIT = EnumHelper.addToolMaterial("material_onit", 4, 2420, 16.0F, 5.0F, 22);
 	public static final ToolMaterial MATERIAL_LAZORITE = EnumHelper.addToolMaterial("material_lazorite", 5, 5000, 40.0F, 6.0F, 30);
+	public static final ItemArmor.ArmorMaterial ORANGE_ESSENCE = EnumHelper.addArmorMaterial("orange_essence", Reference.MOD_ID+":orange_essence",420, new int[] {2,5,5,2}, 30, SoundsHandler.ONIT, 0.0F);
 
 	//Items
 	public static final Item ONIT_ESSENCE = new ItemBase("onit_essence");
@@ -36,7 +43,9 @@ public class ModItems {
 	public static final ToolHoe ONIT_HOE = new ToolHoe("onit_hoe", MATERIAL_ONIT);
 	public static final ToolSpade ONIT_SPADE = new ToolSpade("onit_spade", MATERIAL_ONIT);
 
-
 	public static final ToolPickaxe LAZOR_DRILL = new ToolPickaxe("lazor_drill", MATERIAL_LAZORITE);
+
+	//Armor
+	public static final Item MMGA_HAT = new ArmorBase("mmga_hat", ORANGE_ESSENCE, 1, EntityEquipmentSlot.HEAD);
 
 }
